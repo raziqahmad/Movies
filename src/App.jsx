d5bb955202c2199b5ae34aba5618e216181dbe4c
Raziq Ahmad <raziqamd9695@gmail.com>
2023-10-15 14:54:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import searchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotfound from "./pages/404/PageNotFound";
+import SearchResult from "./pages/searchResult/SearchResult";
+
+function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(() => {
+    fetchapiConfig();
+    genresCall();
+  }, []);
+
+  const fetchapiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
+    });
+  };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoint = ["tv", "movie"];
+    let allGenres = {};
+    endPoint.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotfound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
